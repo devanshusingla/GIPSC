31a3fe9b3cf2015271f281243439faac71dd7ab7
Yatharth Goswami <yatharthgoswami15@gmail.com>
2022-04-25 21:25:49 +0530

    Add newline in prints

--- src/Milestone6/lib/fmt.go
+++ src/Milestone6/lib/fmt.go
@@ -2,16 +2,18 @@ package fmt
 
 var pre int = 3
 
-func Print_int(a int) {
-	__syscall(1, a)
-}
-
 func Print_char(a rune) {
 	__syscall(11, a)
 }
 
+func Print_int(a int) {
+	__syscall(1, a)
+	Print_char(rune(10))
+}
+
 func Print_string(a string) {
 	__syscall(4, a)
+	Print_char(rune(10))
 }
 
 func Scan_int(a *int) {
